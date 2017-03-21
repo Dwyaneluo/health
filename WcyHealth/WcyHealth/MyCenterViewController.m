d48e3edb4c9fee01d6577598e209e2643dc3fc8c
天涯 <tianya@TyMac.local>
2017-03-21 22:52:14 +0800

    update

--- WcyHealth/WcyHealth/MyCenterViewController.m
+++ WcyHealth/WcyHealth/MyCenterViewController.m
@@ -8,8 +8,10 @@
 
 #import "MyCenterViewController.h"
 
-@interface MyCenterViewController ()
-
+@interface MyCenterViewController ()<UITableViewDelegate,UITableViewDataSource>
+{
+    UITableView *table;
+}
 @end
 
 @implementation MyCenterViewController
@@ -17,21 +19,71 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"个人中心";
+    [self createView];
 }
-
+-(void)createView {
+    table=[[UITableView alloc] initWithFrame:CGRectMake(0, 0, ScreenWidth, ScreenHeight)];
+    [self.view addSubview:table];
+    table.delegate=self;
+    table.dataSource=self;
+    [table registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
+    table.tableFooterView =[UIView new];
+    table.backgroundColor = UIColorFromHexValue(0xf4f4f4);
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark- tablview delegate
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 3;
 }
-*/
-
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    if (section==0) {
+        return 3;
+    }else if (section==1){
+        return 2;
+    }else{
+        return 1;
+    }
+}
+-(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    if (section!=0){
+        return 10;
+    }
+    return 0;
+}
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    if (indexPath.section==0&&indexPath.row==0) {
+        return 100;
+    }
+    return 60;
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    NSString * celLStr= @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:celLStr];
+    if (cell!=nil){
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:celLStr];
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        if (indexPath.section==0){
+            if (indexPath.row==0) {
+                cell.textLabel.text = @"点击登陆";
+            }else if(indexPath.row==1){
+                cell.textLabel.text = @"我的报告";
+            }else if(indexPath.row==2){
+                cell.textLabel.text = @"我的订单";
+            }
+        }else if(indexPath.section==1){
+            if (indexPath.row==0) {
+                cell.textLabel.text = @"帮助";
+            }else if(indexPath.row==1){
+                cell.textLabel.text = @"设置";
+            }
+        }else{
+            cell.textLabel.text = @"检查更新";
+        }
+    }
+    return cell;
+}
 @end
